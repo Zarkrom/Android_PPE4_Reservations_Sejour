b7aa51ccb655224eada496db304f9d6a4d589be1
bourasseaun <bourasseaun@gmail.com>
2019-03-29 08:58:48 +0100

    Test

---
+++ app/src/main/java/com/example/marobine/ppe4rservationssjours/gestioncompte_activity.java
@@ -0,0 +1,13 @@
+package com.example.marobine.ppe4rservationssjours;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gestioncompte_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestioncompte_activity);
+    }
+}
