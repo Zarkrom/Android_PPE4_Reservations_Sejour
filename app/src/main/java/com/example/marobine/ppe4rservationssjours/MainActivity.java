dbbcb3360262552d7248b8b6a5b25dceffd26432
marobine <erwann-marobin@hotmail.fr>
2019-04-05 12:13:34 +0200

    Mise à jour

--- app/src/main/java/com/example/marobine/ppe4rservationssjours/MainActivity.java
+++ app/src/main/java/com/example/marobine/ppe4rservationssjours/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inscription);
+        setContentView(R.layout.activity_ajout_enfant);
     }
 }
